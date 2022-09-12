e65ba1f88fc28ef2032c981c09564a52e5a807d6
PaulRomanov <pavel.ramanau@gmail.com>
2022-09-12 23:24:01 +0400

    update header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { NavComponent } from './components/nav/nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    ReactiveFormsModule
 
   ],
   providers: [],
